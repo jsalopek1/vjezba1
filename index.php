c0148955350db576bd01e1b7c0583de0a217aa1f
jsalopek1 <jsalopek@tvz.hr>
2024-10-21 18:13:56 +0200

    Vjezba1

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// PHP blok počinje na početku dokumenta prije deklaracije!
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Davincijev kod">
+    <meta name="keywords" content="Da Vincijev kod, Dan Brown, kriminalistički triler">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+
+    <p>
+        <a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">    <!-- link je neispravan -->
+        Link na wikipediju;
+        </a>
+    </p>
+
+<!-- vjezba 1 - Davincijev kod -->
+
+
+</body>
+</html>
